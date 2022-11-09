26558f403af843e4820b2dea46148c10d55f4424
Justin Hui <jhui@lcs.on.ca>
2022-11-09 09:12:11 -0500

    Initial Commit

---
+++ StopwatchAppInterface/StopwatchAppInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppInterfaceApp.swift
+//  StopwatchAppInterface
+//
+//  Created by Justin Hui on 9/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
